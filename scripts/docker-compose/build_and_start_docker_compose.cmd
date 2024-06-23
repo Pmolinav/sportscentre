e317477bb40a4b79618ed4177b76033da6d3b0cc
pmolinav <pablo7molina97@gmail.com>
2024-06-23 23:32:21 +0200

    SC-15: Updated docker-compose script.

--- scripts/docker-compose/build_and_start_docker_compose.cmd
+++ scripts/docker-compose/build_and_start_docker_compose.cmd
@@ -1,6 +1,26 @@
 REM Building sportscentre image and push it to docker hub
-docker build -t pablo7molina/configurationboapi .
-docker build -t pablo7molina/bookingsservice .
 
-docker push pablo7molina/configurationboapi
-docker push pablo7molina/bookingsservice
+cd ../../backoffice/ConfigurationBOApi
+
+REM Build with Maven (skip tests)
+call mvn clean package -DskipTests
+
+REM Build image
+call docker build -t pablo7molina/configurationboapi .
+
+REM Push image
+call docker push pablo7molina/configurationboapi
+
+cd ../../backoffice/BookingsService
+
+REM Build with Maven (skip tests)
+call mvn clean package -DskipTests
+
+REM Build image
+call docker build -t pablo7molina/bookingsservice .
+
+REM Push image
+call docker push pablo7molina/bookingsservice
+
+REM Start Docker compose
+docker-compose up -d
